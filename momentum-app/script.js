e328e9ce1ffcfa74d1de8c373a9d751768f58534
budzzzzzzzzzzzzzzz <126756459+budzzzzzzzzzzzzzzz@users.noreply.github.com>
2023-03-15 12:46:20 +0800

    first commit

---
+++ momentum-app/script.js
@@ -0,0 +1,185 @@
+
+/*******************************************************************************
+ * *************************SCRIPT FOR MOMENTUM APP*****************************
+ *  Notes:                                                                     *
+ *  1. The concept of my code for to do list is referenced to                  *
+ *  w3schools.com. I have added some modifications to the script and some      *
+ *  features like it can be dragged anywhere.                                  *
+ *  2. The javascript code for weather api is in a different file named        *
+ *  weather.js.                                                                *
+ *  Weather api source: https://api.openweathermap.org/data/2.5/weather?       *
+ * *****************************************************************************/
+
+/**********************Script for displaying time and date**********************/
+
+setInterval(displayTime, 1000);
+
+function displayTime() {
+    let timeDisplay = document.getElementById("time");
+    let timeToday = new Date();
+    //get individual values.
+    let hr = timeToday.getHours();
+    let min = timeToday.getMinutes();
+    let sec = timeToday.getSeconds();
+
+    hr = hr < 10? "0" + hr : hr;
+    min = min < 10? "0" + min : min;
+    sec = sec < 10? "0" + sec : sec;
+    //Time display is in 24hr format.
+    timeDisplay.innerHTML = hr + ':' + min + ':' + sec;
+}
+
+displayTime();
+
+function displayDate() {
+    let todayDisp = document.getElementById("date");
+    let today = new Date;
+    let month = today.getMonth();
+    let day = today.getDate();
+    let year = today.getFullYear();
+    let monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let monthName = monthList[month];
+    day = (day < 10) ? "0" + day : day;
+    todayDisp.innerHTML = monthName + " " + day + ", " + year;
+}
+
+displayDate();
+
+/*--------------End of Script for displaying time and date-------------------*/
+
+/*****************Function for looping background*****************************/
+
+function displayImg() {
+    let images = document.getElementById("chrono");
+    let imgSelection = ["url('images/hammock.jpg')", "url('images/pebbles.jpg')", "url('images/two chairs.jpg')", "url('images/girl.jpg')"];
+    let num = 0;
+    let numImg = imgSelection.length;
+
+    setInterval(function() {
+        images.style.backgroundImage = imgSelection[num++];
+        if (num === numImg) {
+            num = 0;    //refreshes num if reached limit.
+        }
+    }, 60000);  //loop for 60 seconds as per requirement
+
+}
+
+displayImg();
+
+//-------------End of script for looping background image---------------------//
+
+/****************************Script for to do list*****************************/
+
+let addButton = document.querySelector(".add");
+
+// Create a "close" button and append it to each list item
+let myNodelist = document.getElementsByTagName("LI");
+
+for (let i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+//This function creates a new list when add button is pressed.
+function addTask() {
+    let newList = document.createElement("li");
+    let taskInput = document.getElementById("task-input").value;
+    let added = document.createTextNode(taskInput);
+    newList.appendChild(added);
+    if (taskInput === "") {
+        alert("Please Input your task.");
+    }
+    else {
+        document.getElementById("task-lists").appendChild(newList);
+    }
+    document.getElementById("task-input").value = "";
+
+
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    newList.appendChild(span);
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            let thisDiv = this.parentElement;
+            thisDiv.style.display = "none";
+        }
+    }
+}
+
+addButton.addEventListener('click', addTask);
+
+//--------------------End of script for to do list---------------------------//
+
+//*****************This script is for displaying quotes*********************//
+
+function displayQuotes() {
+    let quoteDisp = document.getElementById("quote-text");
+    let quoteLists = ["\"Don't stop just because you're tired, stop when you're done.\"", "\"Life is about making an impact, not making an income.\"", "\"Whatever the mind of man can conceive and believe, it can achieve.\"", "\"Strive not to be a success, but rather to be of value.\"", "\"Two roads diverged in a wood, and I took the one less traveled by, And it has made all the difference.\"", "\"I attribute my success to this: I never gave or took any excuse.\"", "\"Work hard in silence, let the success be the noise!\""];
+    let currList = 0;
+    let numQuotes = quoteLists.length;
+    setInterval(function() {
+
+        quoteDisp.innerHTML = quoteLists[currList++];
+        if (currList === numQuotes) {
+            currList = 0;
+        }
+    }, 5000);
+}
+
+displayQuotes();
+
+//-----------------End of script for displaying quotes------------------------//
+
+
+//*********************Script for dragging elements***************************//
+
+let draggedElem = null;
+
+function dragMove(id) {
+    let toDoWidget = document.getElementById("to-do-list");
+    //this function works when mouse is clicked
+    document.onmousedown = function() {
+        draggedElem = toDoWidget;
+        toDoWidget.style.zIndex = 100000;
+    }
+}
+
+document.onmouseup = function(e) {
+    draggedElem = null; //stop following cursor after the mouse is not pressed
+}
+
+//this function allows the cursor to stay in the div when dragging
+document.onmousemove = function(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    draggedElem.style.top = y - draggedElem.offsetHeight/2 + "px";
+    draggedElem.style.left = x - draggedElem.offsetWidth/2 + "px";
+}
+
+window.onload = dragMove;
+
+//----------------End of script for dragging elements----------------------//
---
+++ momentum-app/weather.js
@@ -0,0 +1,57 @@
+/****************************SCRIPT FOR WEATHER API*****************************/
+
+const api = '9908d691435ad75aebee9e5b9446ab61';
+
+const weatherImage = document.getElementById("weather-icon");
+const locate = document.getElementById("location");
+const descriptionData = document.querySelector(".desc");
+const tempCelsius = document.querySelector(".celsius");
+const sunriseData = document.querySelector(".sunrise");
+const sunsetData = document.querySelector(".sunset");
+
+window.addEventListener('load', () => {
+    let long = 0;
+    let lat = 0;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            //detect actual location
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}`;
+
+
+
+            console.log(base);  //This helps to access full weather data at the console.
+            fetch(base).then((response) => {
+                return response.json();
+            })
+            .then((getData) => {
+                //get specific data from base
+                const {temp} = getData.main;
+                const place = getData.name;
+                const {description, icon} = getData.weather[0];
+                const {sunrise, sunset} = getData.sys;
+
+                const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+
+                const sunriseGMT = new Date(sunrise * 1000); //Convert sunrise time from Epoch to GMT
+                const sunsetGMT = new Date(sunset * 1000);  //Convert sunset time from Epoch to GMT
+
+                weatherImage.src = iconUrl;
+
+                let temptoCelsius = temp - 273.15;  //convert kelvin to celsius
+                //display data
+                locate.textContent = place;
+                descriptionData.textContent = description;
+                tempCelsius.textContent = temptoCelsius.toFixed(2) + "°C";
+                sunriseData.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+                sunsetData.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+            });
+        });
+    }
+});
+
+//------------------END OF SCRIPT FOR WEATHER API-------------------------------//
